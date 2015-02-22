5bc06d6652804aa76c50c5f8c0fc2550db82feb5
Kingsten Banh <kingstenbanh@gmail.com>
2015-02-22 08:53:20 -0700

    add tip calculation methods

---
+++ TipCalculator/TipCalculator/TipCalculatorModel.swift
@@ -0,0 +1,40 @@
+//
+//  TipCalculatorModel.swift
+//  TipCalculator
+//
+//  Created by Kingsten Banh on 2/22/15.
+//  Copyright (c) 2015 Kingsten Banh. All rights reserved.
+//
+
+import Foundation
+
+class TipCalculatorModel {
+    var total: Double
+    var taxPct: Double
+    var subTotal: Double {
+        get {
+            return total / (taxPct + 1)
+        }
+    }
+    
+    init(total: Double, taxPct: Double) {
+        self.total = total
+        self.taxPct = taxPct
+    }
+    
+    func calcTipWithTipPct(tipPct: Double) -> Double {
+        return subTotal * tipPct
+    }
+    
+    func returnPossibleTips() -> [Int: Double] {
+        let possibleTipsInferred = [0.15, 0.18, 0.20]
+        let possibleTipsExplicit:[Double] = [0.15, 0.18, 0.20]
+        
+        var retval = [Int: Double]()
+        for possibleTip in possibleTipsInferred {
+            let intPct = Int(possibleTip * 100)
+            retval[intPct] = calcTipWithTipPct(possibleTip)
+        }
+        return retval
+    }
+}
