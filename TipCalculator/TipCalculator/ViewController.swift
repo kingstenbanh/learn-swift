c1cfb4ffde6a5aae6500ea5c0bb5bfc07b62f872
Kingsten Banh <kingstenbanh@gmail.com>
2015-02-22 23:35:56 -0700

    Swift Tutorial Part 2: A Simple iOS App"

--- TipCalculator/TipCalculator/ViewController.swift
+++ TipCalculator/TipCalculator/ViewController.swift
@@ -10,9 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var totalTextField: UITextField!
+    @IBOutlet var taxPctSlider: UISlider!
+    @IBOutlet var taxPctLabel: UILabel!
+    @IBOutlet var resultsTextView: UITextView!
+    
+    let tipCalc = TipCalculatorModel(total: 33.25, taxPct: 0.06)
+    
+    func refreshUI() {
+        totalTextField.text = String(format: "%0.2f", tipCalc.total)
+        taxPctSlider.value = Float(tipCalc.taxPct) * 100.0
+        taxPctLabel.text = "Tax Percentage (\(Int(taxPctSlider.value))%)"
+        resultsTextView.text = ""
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        refreshUI()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +35,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func calculateTapped(sender: AnyObject) {
+        println("calculate button tapped")
+        tipCalc.total = Double((totalTextField.text as NSString).doubleValue)
+        let possibleTips = tipCalc.returnPossibleTips()
+        var results = ""
+        var keys = Array(possibleTips.keys)
+        sort(&keys)
+        for tipPct in keys {
+            let tipValue = possibleTips[tipPct]!
+            let prettyTipValue = String(format: "%.2f", tipValue)
+            results += "\(tipPct)%: \(prettyTipValue)\n"
+        }
+        resultsTextView.text = results
+    }
+    
+    @IBAction func taxPercentageChanged(sender: AnyObject) {
+        println("Value changed")
+        tipCalc.taxPct = Double(taxPctSlider.value) / 100.0
+        refreshUI()
+    }
+    
+    @IBAction func viewTapped(sender: AnyObject) {
+        println("View tapped")
+        totalTextField.resignFirstResponder()
+    }
 }
 
